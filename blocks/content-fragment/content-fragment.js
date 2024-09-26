6ac9b0dd69eb8fc911293c3100a7b30b04a47ea4
Mihai Indreias <mindreias@adobe.com>
2024-09-26 14:52:21 +0200

    content frag data

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -3,20 +3,14 @@
  * @param {Element} decorate
  */
 export default async function decorate(block) {
-  const aempublishurl = 'https://publish-p130360-e1272151.adobeaemcloud.com';
   const aemauthorurl = 'https://author-p130360-e1272151.adobeaemcloud.com';
-  const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const contentPath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-  const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
+  const variationname = block
+    .querySelector(':scope div:nth-child(2) > div')
+    .innerHTML.trim()
+    .toLowerCase();
 
-  const url =
-    window.location && window.location.origin && window.location.origin.includes('author')
-      ? `${aemauthorurl}${persistedquery};path=${contentPath};variation=${variationname};ts=${
-          Math.random() * 1000
-        }`
-      : `${aempublishurl}${persistedquery};path=${contentPath};variation=${variationname};ts=${
-          Math.random() * 1000
-        }`;
+  const url = `${aemauthorurl}/${contentPath}/jcr:content/data/${variationname}.json`;
   const options = { credentials: 'include' };
 
   const cfReq = await fetch(url, options).then((response) => response.json());

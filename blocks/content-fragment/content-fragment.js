46b4b01a54cab50fe02b730de63350bccb184c43
Mihai Indreias <mindreias@adobe.com>
2024-09-26 15:11:08 +0200

    correct url + css CF

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -4,20 +4,28 @@
  */
 export default async function decorate(block) {
   const aemauthorurl = 'https://author-p130360-e1272151.adobeaemcloud.com';
-  const contentPath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
+  const contentPath = block.querySelector(':scope div:nth-child(1) > div a').textContent.trim();
   const variationname = block
     .querySelector(':scope div:nth-child(2) > div')
-    .innerHTML.trim()
+    .textContent.trim()
     .toLowerCase();
 
-  const url = `${aemauthorurl}/${contentPath}/jcr:content/data/${variationname}.json`;
+  const url = `${aemauthorurl}${contentPath}/jcr:content/data/${variationname}.json`;
   const options = { credentials: 'include' };
 
   const cfReq = await fetch(url, options).then((response) => response.json());
 
   block.innerHTML = `
-        <div>
-            ${JSON.stringify(cfReq)}
-        </div>
-      `;
+  <div class='banner-content'>
+      <div class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${
+        aemauthorurl + cfReq.heroImage
+      });">
+          <p class='pretitle'>${cfReq.headline}</p>
+          <p class='headline'>${cfReq.pretitle}</p>
+          <p class='detail'>${cfReq.detail}</p>
+      </div>
+      <div class='banner-logo'>
+      </div>
+  </div>
+`;
 }
